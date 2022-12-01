d07eed13004d219a9930928c4a43128880a2c72c
Solomonslava <solomonslava1991@gmail.com>
2022-12-01 15:12:01 +0300

    A.V.

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 import json.decoder
-
+import os
 import requests as re
 from settings import email, password
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class Petfriends:
@@ -33,4 +34,49 @@ class Petfriends:
             result = res.text
         return status, result
 
+    def add_new_pet(self, auth_key: json, name: str, animal_type: str, age: str, pet_photo: str) -> json:
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            }
+        )
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
 
+        res = re.post(self.base_url + '/api/pets', headers=headers, data=data)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def del_pet(self,auth_key, pet_id):
+        headers = {'auth_key': auth_key['key']}
+
+        res = re.delete(self.base_url + '/api/pets/' + pet_id, headers=headers)
+        status = res.status_code
+
+        return status
+
+    def update_pet_info(self, auth_key, pet_id, name, animal_type, age):
+        data = MultipartEncoder(
+            {
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+            }
+        )
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = re.put(self.base_url + '/api/pets/' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,5 +1,6 @@
 from api import Petfriends
 from settings import email, password
+import os
 
 pf = Petfriends()
 
@@ -13,3 +14,36 @@ def test_get_list_of_pets(filter=''):
     status, result = pf.get_list_of_pets(auth_key, filter)
     assert status == 200
     assert len(result['pets']) > 0
+
+def test_add_new_pet_correct(name='storm', animal_type='cat', age='10', pet_photo='images/britanskaya.jpg'):
+    _, auth_key = pf.get_api_key(email=email, password=password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['name'] == name
+
+def test_del_pet_correct():
+    _, auth_key = pf.get_api_key(email=email, password=password)
+    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    if len(list_of_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, name='vasya', animal_type='cat', age='10', pet_photo='images/britanskaya.jpg')
+        _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    pet_id = list_of_pets['pets'][0]['id']
+    status = pf.del_pet(auth_key, pet_id)
+    assert status == 200
+    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    assert list_of_pets['pets'][0]['id'] != pet_id
+
+
+
+def test_update_pet_info_correct(name='lion', animal_type='bear', age='3'):
+    _, auth_key = pf.get_api_key(email, password)
+    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    if len(list_of_pets['pets']) > 0:
+        pet_id = list_of_pets['pets'][0]['id']
+        status, result = pf.update_pet_info(auth_key, pet_id, name, animal_type, age)
+        assert status == 200
+        assert result['name'] == name
+        assert result['age'] == age
+        assert result['animal_type'] == animal_type
+    else:
+        raise Exception('There is no my pets')
