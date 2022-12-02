c051ba764c37f3e8ee636929a8190a72514b7ed7
Solomonslava <solomonslava1991@gmail.com>
2022-12-02 08:57:30 +0300

    a.v.

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -9,16 +9,33 @@ def test_get_api_key_correct(email=email, password=password):
     assert status == 200
     assert 'key' in result
 
-def test_get_api_key_filed(email='asdff@mail.ru', password='asdffa'):
-    status, result = pf.get_api_key(email, password)
-    assert status == 200
-    assert 'key' not in result
+#def test_get_api_key_incorrect_password(email=email, password='asdffa'):
+ #   status, result = pf.get_api_key(email, password)
+  #  assert status == 200
+   # assert 'key' not in result
 
-def test_get_list_of_pets(filter=''):
+#def test_get_api_key_incorrect_email(email='asheqwr.ru, password='asdffa'):
+ #   status, result = pf.get_api_key(email, password)
+  #  assert status == 200
+   # assert 'key' not in result
+
+def test_get_list_of_pets_all(filter=''):
     _, auth_key = pf.get_api_key(email=email, password=password)
     status, result = pf.get_list_of_pets(auth_key, filter)
     assert status == 200
     assert len(result['pets']) > 0
+
+#def test_get_list_of_pets_my_pets(filter='my_pets'):
+ #   _, auth_key = pf.get_api_key(email=email, password=password)
+  #  status, result = pf.get_list_of_pets(auth_key, filter)
+   # assert status == 200
+    #assert len(result['pets']) > 0
+
+#def test_get_list_of_pets_all_incorrect_auth_key(filter=''):
+ #   auth_key = 'asafd548dsf84ds21f6ds8'
+  #  status, result = pf.get_list_of_pets(auth_key, filter)
+   # assert status == 200
+    #assert len(result['pets']) > 0
 
 def test_add_new_pet_correct(name='storm', animal_type='cat', age='10', pet_photo='images/britanskaya.jpg'):
     _, auth_key = pf.get_api_key(email=email, password=password)
@@ -26,18 +43,47 @@ def test_add_new_pet_correct(name='storm', animal_type='cat', age='10', pet_phot
     assert status == 200
     assert result['name'] == name
 
-def test_del_pet_correct():
+#def test_add_new_pet_without_name(name='', animal_type='cat', age='10', pet_photo='images/britanskaya.jpg'):
+  #  _, auth_key = pf.get_api_key(email=email, password=password)
+  #  status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+   # assert status == 200
+    #assert result['name'] == name
+
+#def test_add_new_pet_without_animal_type(name='storm', animal_type='', age='10', pet_photo='images/britanskaya.jpg'):
+  #  _, auth_key = pf.get_api_key(email=email, password=password)
+  #  status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+   # assert status == 200
+    #assert result['name'] == name
+
+#def test_add_new_pet_without_age(name='', animal_type='cat', age='', pet_photo='images/britanskaya.jpg'):
+  #  _, auth_key = pf.get_api_key(email=email, password=password)
+  #  status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+   # assert status == 200
+    #assert result['name'] == name
+
+def test_del_my_pet_correct():
     _, auth_key = pf.get_api_key(email=email, password=password)
-    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='my_pets')
     if len(list_of_pets['pets']) == 0:
         pf.add_new_pet(auth_key, name='vasya', animal_type='cat', age='10', pet_photo='images/britanskaya.jpg')
-        _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+        _, list_of_pets = pf.get_list_of_pets(auth_key, filter='my_pets')
     pet_id = list_of_pets['pets'][0]['id']
     status = pf.del_pet(auth_key, pet_id)
     assert status == 200
-    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    _, list_of_pets = pf.get_list_of_pets(auth_key, filter='my_pets')
     assert list_of_pets['pets'][0]['id'] != pet_id
 
+#def test_del_enemy_pet_correct():
+ #   _, auth_key = pf.get_api_key(email=email, password=password)
+  #  _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+  #  if len(list_of_pets['pets']) == 0:
+  #      pf.add_new_pet(auth_key, name='vasya', animal_type='cat', age='10', pet_photo='images/britanskaya.jpg')
+  #      _, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+   # pet_id = list_of_pets['pets'][0]['id']
+    #status = pf.del_pet(auth_key, pet_id)
+    #assert status == 200
+    #_, list_of_pets = pf.get_list_of_pets(auth_key, filter='')
+    #assert list_of_pets['pets'][0]['id'] != pet_id
 
 
 def test_update_pet_info_correct(name='lion', animal_type='bear', age='3'):
